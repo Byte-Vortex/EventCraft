819c239662856b03e3d17bf743b7bb49bad1d062
Akshat Saini <akshatanwar24@gmail.com>
2024-11-12 11:26:29 +0530

    update

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -59,13 +59,12 @@ export default function Gallery() {
           </motion.div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-3 ">
-            
-              {galleryItems.map((item, index) => (
-                <motion.div
+            {galleryItems.map((item, index) => (
+              <motion.div
                 key={index}
-                initial={{ opacity: 0, x: -50 }}
+                initial={{ opacity: 0, x: 0 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.7, delay: 0.3+index*0.3 }}
+                transition={{ duration: 0.7, delay: 0.3 + index * 0.3 }}
                 className="text-center"
               >
                 <div key={index} className="relative group ">
@@ -85,10 +84,8 @@ export default function Gallery() {
                     </div>
                   </Link>
                 </div>
-                </motion.div>
-                
-              ))}
-            
+              </motion.div>
+            ))}
           </div>
         </div>
       </main>
